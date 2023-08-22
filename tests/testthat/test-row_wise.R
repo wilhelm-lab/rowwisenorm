7e7054ff76d422cb7526ea2b46845b6be46dfae5
sinamo24 <138229031+sinamo24@users.noreply.github.com>
2023-08-22 23:16:23 +0200

    modified

--- tests/testthat/test-row_wise.R
+++ tests/testthat/test-row_wise.R
@@ -3,9 +3,21 @@ library(testthat)        # load testthat package
 library(rowwisenorm)       # load our package
 
 test_that("read_files() returns a list of length 3", {
-  returned_list <- read_files("proteinGroups.txt", "experimentalDesign.txt")
+  data_path1 <- system.file("extdata", "proteinGroups.txt", package = "rowwisenorm")
+  data_path2 <- system.file("extdata", "experimentalDesign.txt", package = "rowwisenorm")
+
+  returned_list <- read_files(data_path1, data_path2)
   expect_equal(length(returned_list), 3)
 })
 
 # TODO add more tests
+
+
+# --> call 'devtools::test()' in console to run these tests
+# csv files need to be stored in inst/extdata and not in /data, in /data use the rda format for any files
+
+# --> call 'devtools::check()' to check if package works on various operating systems
+# -> this shows what exactly needs to be modified in the code
+
+# --> call 'rhub::check_for_cran()' to check for CRAN specific requirements
 
