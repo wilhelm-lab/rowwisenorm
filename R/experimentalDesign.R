adbda99df7ac9312d730c7bc8564cb6ebac23d1a
sinamo24 <138229031+sinamo24@users.noreply.github.com>
2023-08-23 11:39:51 +0200

    changing the repo

---
+++ R/experimentalDesign.R
@@ -0,0 +1,16 @@
+#' Experimental design file matching the MaxQuant output file proteinGroups.txt
+#'
+#' @docType data
+#'
+#' @usage read_files("proteinGroups.txt", "experimentalDesign.txt")
+#'
+#' @format An object of class \code{"data.frame"}
+#' \describe{
+#'  Contains the conditions inside the first column and all column names inside
+#'  the MaxQuant file that are desired for the lowest-level data frame
+#' }
+#'
+#' @references reference
+#'
+#' @keywords dataset
+#'
---
+++ R/proteinGroups.R
@@ -0,0 +1,15 @@
+#' MaxQuant output file proteinGroups.txt
+#'
+#' @docType data
+#'
+#' @usage read_files("proteinGroups.txt", "experimentalDesign.txt")
+#'
+#' @format An object of class \code{"data.frame"}
+#' \describe{
+#'  Contains all columns of the MaxQuant output
+#' }
+#'
+#' @references reference
+#'
+#' @keywords dataset
+#'
