803018fbb124475fd2854ebdf896d991c37871c7
sinamo24 <138229031+sinamo24@users.noreply.github.com>
2023-08-25 13:48:28 +0200

    added '...' back

--- R/panel.smooth2.R
+++ R/panel.smooth2.R
@@ -13,9 +13,10 @@
 #' @param col.smooth color to be used by lines for drawing the smooths
 #' @param span smoothing parameter f for lowess()
 #' @param iter number of robustness iterations for lowess()
+#' @param ... further arguments
 #'
 
-panel.smooth2 <- function(x, y, col = par("col"), bg = NA, pch = ".", cex = 1, col.smooth = "red", span = 2/3, iter = 3) {
+panel.smooth2 <- function(x, y, col = par("col"), bg = NA, pch = ".", cex = 1, col.smooth = "red", span = 2/3, iter = 3, ...) {
   idxs <- sample(1:length(x), max(length(x)*0.10, min(1000, length(x))))
   points(x[idxs], y[idxs], pch = pch, col = col, bg = bg, cex = cex)
   ok <- is.finite(x) & is.finite(y)
