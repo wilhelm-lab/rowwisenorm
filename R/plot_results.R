7e0fba4842aa3a3f5ee21d1cfa8a647ced21c4ef
sinamo24 <138229031+sinamo24@users.noreply.github.com>
2023-08-25 13:52:14 +0200

    new default, file and dir exactly named as users entry now, used trimws()

--- R/plot_results.R
+++ R/plot_results.R
@@ -10,22 +10,27 @@
 #' @return Creates an output pdf file containing the generated plots
 #'
 
-plot_results <- function(lowest_level_df, main="", output_dir="results"){
+plot_results <- function(lowest_level_df, main="", output_dir=""){
   intensities <- lowest_level_df[, !colnames(lowest_level_df) %in% "row.number"]
 
+  # important: trim white space at start and end of users entry
+  main <- trimws(main)
+  output_dir <- trimws(output_dir)
+
   # plots
-  filename <- "results"
-  if (main != ""){
-    main_name <- gsub("\\s+", "_", main)  # _ instead of white space
-    filename <- paste(filename, main_name, sep = "_")
+  if (main == ""){
+    filename <- "result_rowwisenorm"
   }
+  else {
+    filename <- main
+  }
+
   filename <- paste(filename, ".pdf", sep = "")
 
-  if(! dir.exists(output_dir)){
-    dir.create(output_dir)
-  }
-
-  if (trimws(output_dir) != ""){
+  if (output_dir != ""){
+    if(! dir.exists(output_dir)){
+      dir.create(output_dir)
+    }
     filename <- paste0(output_dir, "/", filename, sep="")
   }
 
