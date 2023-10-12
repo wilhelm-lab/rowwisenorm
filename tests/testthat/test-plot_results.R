6d6ad765b9d317bc2fcfd5fd157938048139a643
sinamo24 <138229031+sinamo24@users.noreply.github.com>
2023-10-12 22:02:44 +0200

    updated

--- tests/testthat/test-plot_results.R
+++ tests/testthat/test-plot_results.R
@@ -10,8 +10,9 @@ test_that("plot_results() creates PDF file correctly in default location", {
 
   return_list <- read_files(data_path1, data_path2)
   lowest_level_df <- return_list[["lowest_level_df"]]
+  exp_design <- return_list[["exp_design"]]
 
-  plot_results(lowest_level_df = lowest_level_df)
+  plot_results(lowest_level_df = lowest_level_df, exp_design = exp_design)
   expected_file <- "result_rowwisenorm.pdf"
   expect_true(file.exists(expected_file))
 })
@@ -22,8 +23,9 @@ test_that("plot_results() creates PDF file correctly with specified name in spec
 
   return_list <- read_files(data_path1, data_path2)
   lowest_level_df <- return_list[["lowest_level_df"]]
+  exp_design <- return_list[["exp_design"]]
 
-  plot_results(lowest_level_df = lowest_level_df, main = "my_results", output_dir = "res")
+  plot_results(lowest_level_df = lowest_level_df, exp_design = exp_design, main = "my_results", output_dir = "res")
   expected_file <- "res/my_results.pdf"
   expect_true(file.exists(expected_file))
 })
