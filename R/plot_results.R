699a7ce1b7d79e5ad0c915750620c262075c84ab
sinamo24 <138229031+sinamo24@users.noreply.github.com>
2023-10-12 22:02:14 +0200

    updated, added parameter

--- R/plot_results.R
+++ R/plot_results.R
@@ -4,8 +4,11 @@
 #' Function to be called to generate all plots and save them inside an output pdf file
 #'
 #' @param lowest_level_df lowest-level data frame
+#' @param exp_design experimental design data frame
 #' @param main optionally specify the name of the output file and title for the plots
 #' @param output_dir optionally specify the directory in which the output file will be saved
+#' @param show_labels states whether the labels for the data points are shown inside the PCA plot
+#' @param legend_shift states how much the second legend is shifted to the right inside the PCA plot inside PDF
 #'
 #' @return Creates an output pdf file containing the generated plots
 #'
@@ -13,8 +16,8 @@
 #' @importFrom grDevices dev.off pdf
 #'
 
-plot_results <- function(lowest_level_df, main="", output_dir=""){
-  intensities <- lowest_level_df[, !colnames(lowest_level_df) %in% "row.number"]
+plot_results <- function(lowest_level_df, exp_design, main="", output_dir="", show_labels=T, legend_shift=20){
+  data <- lowest_level_df[, !colnames(lowest_level_df) %in% "row.number"]
 
   # important: trim white space at start and end of users entry
   main <- trimws(main)
@@ -38,6 +41,7 @@ plot_results <- function(lowest_level_df, main="", output_dir=""){
   }
 
   pdf(filename, width=10, height=10)
-  plotStats(intensities, main=main)
+  plotStats(data, exp_design, main=main, show_labels=show_labels, legend_shift=legend_shift, pdf_mode=T)  # set here pdf_mode True for adjusted legends
+
   dev.off()
 }
