53f4a61a2db507687bea3abd20df6e5d7b552d38
sinamo24 <138229031+sinamo24@users.noreply.github.com>
2023-10-20 19:34:36 +0200

    removed outcommented lines and parameter legend_shift - final

--- R/pcaPlot2.R
+++ R/pcaPlot2.R
@@ -7,7 +7,6 @@
 #' @param exp_design experimental design data frame
 #' @param main title for the plot
 #' @param show_labels states whether the labels for the data points are shown
-#' @param legend_shift states how much the second legend is shifted to the right inside PDF
 #' @param pdf_mode used for adjusting the plot for the PDF when generated in plot_results
 #'
 #' @return creates a PCA plot
@@ -17,10 +16,7 @@
 #' @importFrom graphics lines pairs par points strwidth text legend
 #'
 
-# TODO remove legend_shift parameter
-# -> do not use it, always do legends the same way (as currently)
-
-pcaPlot2 <- function(data, exp_design, main="", show_labels=F, legend_shift=20, pdf_mode=F) {
+pcaPlot2 <- function(data, exp_design, main="", show_labels=F, pdf_mode=F) {
   data <- data[, !colnames(data) %in% "row.number"]
 
   number_batches <- ncol(exp_design) -1
@@ -32,7 +28,7 @@ pcaPlot2 <- function(data, exp_design, main="", show_labels=F, legend_shift=20, 
   for (i in 1:number_batches){
     var_name <- paste("batch", i, sep="")
     var_value <- exp_design[, i+1]  # batch 1 is stored in column 2
-    var_value <- gsub(" ", ".", var_value)  # again make the names in exp design and column names match
+    var_value <- gsub(" ", ".", var_value)  # TODO remove: again make the names in exp design and column names match
     var_value <- gsub("\\(", ".", var_value)
     var_value <- gsub("\\)", ".", var_value)
     var_value <- gsub("-", ".", var_value)
@@ -73,30 +69,6 @@ pcaPlot2 <- function(data, exp_design, main="", show_labels=F, legend_shift=20, 
   symbols <- rep(my_symbols, vec11)
   column_symbols <- symbols[match(names(data), vec22)]
 
-  # check
-  # print("vec1")
-  # print(vec1)
-  # print("vec2")
-  # print(vec2)
-  # print("vec3")
-  # print(vec3)
-  # print("colors")
-  # print(colors)
-  # print("column_colors")
-  # print(column_colors)
-  #
-  # print("----------")
-  # print("vec11")
-  # print(vec11)
-  # print("vec22")
-  # print(vec22)
-  # print("vec33")
-  # print(vec33)
-  # print("symbols")
-  # print(symbols)
-  # print("column_symbols")
-  # print(column_symbols)
-
   if(pdf_mode){
     par(mar = c(15, 15, 15, 15), xpd = TRUE) # set margins before plot
   }
@@ -124,132 +96,23 @@ pcaPlot2 <- function(data, exp_design, main="", show_labels=F, legend_shift=20, 
        col = column_colors,  # added
        pch = column_symbols,  # added
        main=main,
-       #xlab=paste("PCA1 (",round((max(fit2$x[,1])-min(fit2$x[,1])), digits=1),"% )"),
-       #ylab=paste("PCA2 (",round((max(fit2$x[,2])-min(fit2$x[,2])), digits=1),"% )"),
        xlab=my_xlab,  # added
        ylab=my_ylab,  # added
-       #pch=7,
        cex=1.5,
        xlim = c(min(fit2$x[,1]+(min(fit2$x[,1])*0.1)),max(fit2$x[,1]+(max(fit2$x[,1])*0.1))),
        ylim = c(min(fit2$x[,2]+(min(fit2$x[,2])*0.1)),max(fit2$x[,2]+(max(fit2$x[,2])*0.1))))
   text(fit2$x[,1],fit2$x[,2], labels=my_labels, font=2, pos=1, lwd=2, cex=1.5, offset=1)  # show the labels depending on user's choice
 
-  # legends
-  # legend("topright", title = "Batches", inset = c(-0.3, 0.0), legend = vec3, col = my_colors, pch = 19)  # added
-  # legend("topright", title = "Conditions", inset = c(-0.3, 0.0), legend = vec33, col = "black", pch = my_symbols)  # added
-
   usr <- par("usr")
   greatest_x <- max(fit2$x[,1])  # rightest value of plot, depending on individual data
-  right_shift <- ( max(fit2$x[,1]) - min(fit2$x[,1]) ) * 0.1  # 10 % of distance of greatest and smallest value on x axis
+  right_shift <- ( max(fit2$x[,1]) - min(fit2$x[,1]) ) * 0.15  # 15 % of distance of greatest and smallest value on x axis
   # - alternatively just set a number as right_shift e.g. always 10 units next to greatest x value
-  legend_width <- strwidth("xxBatch100xx")  # assume legend with at most 999 batches plus borders (width ~19)
+  legend_width <- strwidth("xxBatch100xx")  # assume first legend with at most 999 batches + borders (width ~19)
 
-  # first legend:  10% of distance on x axis right to greatest x value (e.g. x going -50 until 100, this is 10% of 150 = 15 units shift)
+  # first legend:  15 % of distance on x axis right to greatest x value (e.g. x going -50 until 100, this is 10% of 150 = 15 units shift, 15 % is 22,5 units)
   legend(x=greatest_x + right_shift, y=usr[4], title = "Batches", inset = c(-0.3, 0.0), legend = vec3, col = my_colors, pch = 19)  # added
 
   # second legend:  shifted next to other legend with shift being the width of first legend
   legend(x=greatest_x + right_shift + legend_width, y=usr[4], title = "Conditions", inset = c(-0.3, 0.0), legend = vec33, col = "black", pch = my_symbols)  # added
-
-
-
-  # alternatively: separate for pdf mode (both legends right side and shift parameter)
-  # usr <- par("usr")
-  # if(pdf_mode){
-  #   # Set the x and y coordinates for the legend relative to the user coordinates
-  #   x_legend <- usr[2] # right border
-  #   y_legend <- usr[4] # top border
-  #   legend(x = x_legend, y = y_legend, title = "Batches", inset = c(-0.3, 0.0), legend = vec3, col = my_colors, pch = 19)  # added
-  #
-  #   # Update the x-coordinate and the y-coordinate for the second legend
-  #   x_legend <- usr[2] + legend_shift # right border with shift parameter
-  #   y_legend <- usr[4] # top border
-  #   legend(x = x_legend, y = y_legend, title = "Conditions", inset = c(-0.3, 0.0), legend = vec33, col = "black", pch = my_symbols)  # added
-  #
-  # }
-  # else {
-  #   legend("bottomleft", title = "Batches", inset = c(-0.3, 0.0), legend = vec3, col = my_colors, pch = 19)  # added
-  #   legend("topright", title = "Conditions", inset = c(-0.3, 0.0), legend = vec33, col = "black", pch = my_symbols)  # added
-  # }
-
-
-  # new ggplot
-
-  # pca_plot <- ggplot(as.data.frame(t(data)), aes(x = fit2$x[,1], y = fit2$x[,2], label = my_labels)) +
-  #   geom_point(aes(color = as.factor(column_colors), shape = as.factor(column_symbols), size = 3)) +
-  #   theme_minimal() +
-  #   labs(title = main, x = my_xlab, y = my_ylab) +
-  #   scale_color_manual(values = as.factor(column_colors)) +
-  #   scale_shape_manual(values = as.factor(column_symbols)) +
-  #   xlim(c(min(fit2$x[,1]) - 0.1, max(fit2$x[,1]) + 0.1)) +
-  #   ylim(c(min(fit2$x[,2]) - 0.1, max(fit2$x[,2]) + 0.1)) +
-  #   geom_text(hjust = 0, vjust = 0) +
-  #   theme(legend.position = "none")  # Remove automatic legend
-  #
-  # print(pca_plot)
-
-  # Create a data frame with PCA results and custom aesthetics
-  # pca_data <- data.frame(
-  #   PC1 = fit2$x[,1],
-  #   PC2 = fit2$x[,2],
-  #   Group = column_colors,  # Use your custom colors
-  #   Symbol = column_symbols,  # Use your custom symbols,
-  #   Label = my_labels
-  # )
-  #
-  # # Convert custom colors and symbols to factors
-  # pca_data$Group <- as.factor(pca_data$Group)
-  # pca_data$Symbol <- as.factor(pca_data$Symbol)
-  #
-  # # Create a PCA plot with ggplot2
-  # pca_plot <- ggplot(pca_data, aes(x = PC1, y = PC2, label = Label, color = Group, shape = Symbol)) +
-  #   geom_point(size = 3) +
-  #   theme_minimal() +
-  #   labs(title = main, x = my_xlab, y = my_ylab) +
-  #   scale_color_manual(values = unique(pca_data$Group)) +
-  #   scale_shape_manual(values = unique(pca_data$Symbol)) +
-  #   xlim(c(min(pca_data$PC1) - 0.1, max(pca_data$PC1) + 0.1)) +
-  #   ylim(c(min(pca_data$PC2) - 0.1, max(pca_data$PC2) + 0.1)) +
-  #   geom_text(hjust = 0, vjust = 0) +
-  #   theme(legend.position = "none")  # Remove automatic legend
-  #
-  # # Add legends manually
-  # pca_plot +
-  #   annotate("text", x = -2.5, y = -4, label = "Batches", size = 5) +
-  #   annotate("text", x = 3.5, y = 4, label = "Conditions", size = 5) +
-  #   scale_shape_manual(name = "Batches", values = column_symbols, labels = vec3) +
-  #   scale_color_manual(name = "Conditions", values = column_colors, labels = vec33)
-
-  # NEW
-
-  # # Create a data frame containing PCA results, labels, and factors for Batches and Conditions
-  # pca_data <- data.frame(
-  #   PC1 = fit2$x[, 1],
-  #   PC2 = fit2$x[, 2],
-  #   Label = ifelse(show_labels, rownames(fit2$x), ""),
-  #   Batch = factor(column_colors, levels = unique(column_colors)),
-  #   Condition = factor(column_symbols, levels = unique(column_symbols))
-  # )
-  #
-  # # Create a PCA plot using ggplot2
-  # pca_plot <- ggplot(pca_data, aes(x = PC1, y = PC2, label = Label, color = Batch, shape = Condition)) +
-  #   geom_point(size = 3) +
-  #   theme_minimal() +
-  #   labs(
-  #     title = main,
-  #     x = my_xlab,
-  #     y = my_ylab
-  #   ) +
-  #   xlim(c(min(pca_data$PC1) - 0.1, max(pca_data$PC1) + 0.1)) +
-  #   ylim(c(min(pca_data$PC2) - 0.1, max(pca_data$PC2) + 0.1)) +
-  #   geom_text(hjust = 0, vjust = 0, size = 5)   # Adjust the size of labels
-  #
-  # # Add legends for Batches and Conditions
-  # pca_plot <- pca_plot +
-  # scale_color_manual(name = "Batches", values = my_colors) +
-  # scale_shape_manual(name = "Conditions", values = my_symbols) +
-  # theme(legend.position = c(0.02, 0.98))  # Adjust the legend position
-  #
-  # # Display the PCA plot
-  # print(pca_plot)
 
 }
