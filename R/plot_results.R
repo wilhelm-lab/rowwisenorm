53de793b1df237968fef186ee69c87c1e7c7af72
sinamo24 <138229031+sinamo24@users.noreply.github.com>
2023-10-21 13:09:44 +0200

    added svg parameter, saves each plot in own svg additionally to PDF

--- R/plot_results.R
+++ R/plot_results.R
@@ -8,6 +8,7 @@
 #' @param main optionally specify the name of the output file and title for the plots
 #' @param output_dir optionally specify the directory in which the output file will be saved
 #' @param show_labels states whether the labels for the data points are shown inside the PCA plot
+#' @param svg states whether a svg file is created for each of the plots additionally
 #'
 #' @return Creates an output pdf file containing the generated plots
 #'
@@ -15,7 +16,7 @@
 #' @importFrom grDevices dev.off pdf
 #'
 
-plot_results <- function(lowest_level_df, exp_design, main="", output_dir="", show_labels=F){
+plot_results <- function(lowest_level_df, exp_design, main="", output_dir="", show_labels=F, svg=F){
   data <- lowest_level_df[, !colnames(lowest_level_df) %in% "row.number"]
 
   # important: trim white space at start and end of users entry
@@ -30,37 +31,31 @@ plot_results <- function(lowest_level_df, exp_design, main="", output_dir="", sh
     filename <- main
   }
 
-  # filename_pdf <- paste(filename, ".pdf", sep = "")
-  # filename_svg <- paste(filename, ".svg", sep = "")
-
   if (output_dir != ""){
     if(! dir.exists(output_dir)){
       dir.create(output_dir)
     }
     filename <- paste0(output_dir, "/", filename, sep="")
-    # filename_pdf <- paste0(output_dir, "/", filename_pdf, sep="")
-    # filename_svg <- paste0(output_dir, "/", filename_svg, sep="")
-
   }
 
-  # wanted_devices <- c("pdf", "svg")  # could also do png here
-  #
-  # plotStats(data, exp_design, main=main, show_labels=show_labels, legend_shift=legend_shift, pdf_mode=T)  # set here pdf_mode True for adjusted legends
-  #
-  # # Loop over all devices and copy the plots there
-  # for (device in wanted_devices) {
-  #   dev.copy(
-  #     eval(parse(text = device)),
-  #     paste(filename, device, sep = ".")
-  #   )
-  #   dev.off()
-  # }
-
-  pdf(filename, width=10, height=10)
-  plotStats(data, exp_design, main=main, show_labels=show_labels, pdf_mode=T)  # set here pdf_mode True for adjusted margins
+  # PDF
+  pdf(paste(filename, "pdf", sep = "."), width = 10, height = 10)  # Set PDF-specific options
+  plotStats(data, exp_design, main=main, show_labels=show_labels, pdf_mode=T)  # set here pdf_mode T
   dev.off()
 
-  # svg(filename_svg, width = 10, height = 10)
-  # plotStats(data, exp_design, main=main, show_labels=show_labels, legend_shift=legend_shift, pdf_mode=T)  # set here pdf_mode True for adjusted legends
+  # SVG
+  if(svg){
+    svg_filename <- paste(filename, "%02d.svg", sep="")  # to get multiple files, e.g. "myplots%02d.svg"
+    svg(svg_filename, width = 10, height = 10)  # Set SVG-specific options
+    plotStats(data, exp_design, main=main, show_labels=show_labels, pdf_mode=F)  # set here pdf_mode F
+    dev.off()
+  }
+
+  # * for only PDF
+  # filename <- paste(filename, ".pdf", sep = "")  # TODO remove this when applying multiple file formats
+
+  # pdf(filename, width=10, height=10)
+  # plotStats(data, exp_design, main=main, show_labels=show_labels, pdf_mode=T)  # set here pdf_mode True for adjusted margins
   # dev.off()
+
 }
