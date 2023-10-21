d4a5a08e8d3c3616ab61f370b6c7283aed09b8c6
sinamo24 <138229031+sinamo24@users.noreply.github.com>
2023-10-21 21:53:55 +0200

    more comments

--- R/pcaPlot2.R
+++ R/pcaPlot2.R
@@ -27,17 +27,18 @@ pcaPlot2 <- function(data, exp_design, main="", show_labels=F, pdf_mode=F) {
   vec3 <- c()
   for (i in 1:number_batches){
     var_name <- paste("batch", i, sep="")
-    var_value <- exp_design[, i+1]  # batch 1 is stored in column 2
+    var_value <- exp_design[, i+1]  # batch 1 is stored in column 2 etc
+    var_value <- var_value[!grepl("^\\s*$", var_value)]  # exclude missing fields
 
-    vec1 <- append(vec1, length(var_value))  # maybe check when a missing field inside exp design -> filter empty (white space) places out of var_value before?
-    vec2 <- append(vec2, var_value)
-    vec3 <- append(vec3, var_name)
+    vec1 <- append(vec1, length(var_value))  # number of conditions for each batch
+    vec2 <- append(vec2, var_value)  # column names inside data for each batch
+    vec3 <- append(vec3, var_name)  # batch1, batch2 etc
   }
 
   my_colors <- sample(colors(), number_batches)  # random pick as much colors as batches
 
-  colors <- rep(my_colors, vec1)
-  column_colors <- colors[match(names(data), vec2)]
+  colors <- rep(my_colors, vec1)  # repeat each of the chosen colors as often as there are conditions for each of the batches
+  column_colors <- colors[match(names(data), vec2)]  # assign to column names of data
 
   vec11 <- c()
   vec22 <- c()
@@ -45,10 +46,11 @@ pcaPlot2 <- function(data, exp_design, main="", show_labels=F, pdf_mode=F) {
   for (i in 1:number_conds){
     var_name <- exp_design[i, 1]  # name of the cond
     var_value <- exp_design[i, 2:ncol(exp_design)]
+    var_value <- var_value[!grepl("^\\s*$", var_value)]  # exclude missing fields
 
-    vec11 <- append(vec11, length(var_value))  # maybe check when a missing field inside exp design -> filter empty (white space) places out of var_value before?
-    vec22 <- append(vec22, var_value)
-    vec33 <- append(vec33, var_name)
+    vec11 <- append(vec11, length(var_value))  # number of batches for each condition (row i)
+    vec22 <- append(vec22, var_value)  # column names inside data for each condition
+    vec33 <- append(vec33, var_name)  # names of the conditions
   }
 
   my_symbols <- sample(1:18, number_conds)
@@ -67,7 +69,7 @@ pcaPlot2 <- function(data, exp_design, main="", show_labels=F, pdf_mode=F) {
   fit2 <- prcomp(t(data))
 
   if(show_labels){
-    my_labels <- rownames(fit2$x)  # column names of the data
+    my_labels <- rownames(fit2$x)  # column names of the data (data got transposed in prcomp)
   }
   else {
     my_labels <- ""
@@ -86,20 +88,19 @@ pcaPlot2 <- function(data, exp_design, main="", show_labels=F, pdf_mode=F) {
        xlab=my_xlab,  # added
        ylab=my_ylab,  # added
        cex=1.5,
-       xlim = c(min(fit2$x[,1]+(min(fit2$x[,1])*0.1)),max(fit2$x[,1]+(max(fit2$x[,1])*0.1))),
+       xlim = c(min(fit2$x[,1]+(min(fit2$x[,1])*0.1)),max(fit2$x[,1]+(max(fit2$x[,1])*0.1))),  # buffer for axes: 10% of the minimum value added to the minimum value, analog for maximum
        ylim = c(min(fit2$x[,2]+(min(fit2$x[,2])*0.1)),max(fit2$x[,2]+(max(fit2$x[,2])*0.1))))
   text(fit2$x[,1],fit2$x[,2], labels=my_labels, font=2, pos=1, lwd=2, cex=1.5, offset=1)  # show the labels depending on user's choice
 
   usr <- par("usr")
   greatest_x <- max(fit2$x[,1])  # rightest value of plot, depending on individual data
   right_shift <- ( max(fit2$x[,1]) - min(fit2$x[,1]) ) * 0.15  # 15 % of distance of greatest and smallest value on x axis
-  # - alternatively just set a number as right_shift e.g. always 10 units next to greatest x value
   legend_width <- strwidth("xxBatch100xx")  # assume first legend with at most 999 batches + borders (width ~19)
 
   # first legend:  15 % of distance on x axis right to greatest x value (e.g. x going -50 until 100, this is 10% of 150 = 15 units shift, 15 % is 22,5 units)
-  legend(x=greatest_x + right_shift, y=usr[4], title = "Batches", inset = c(-0.3, 0.0), legend = vec3, col = my_colors, pch = 19)  # added
+  legend(x=greatest_x + right_shift, y=usr[4], title = "Batches", legend = vec3, col = my_colors, pch = 19)  # add inset = c(-0.3, 0.0), to shift a bit up
 
   # second legend:  shifted next to other legend with shift being the width of first legend
-  legend(x=greatest_x + right_shift + legend_width, y=usr[4], title = "Conditions", inset = c(-0.3, 0.0), legend = vec33, col = "black", pch = my_symbols)  # added
+  legend(x=greatest_x + right_shift + legend_width, y=usr[4], title = "Conditions", legend = vec33, col = "black", pch = my_symbols)
 
 }
