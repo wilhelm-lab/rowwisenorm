06540a3b603d0f1d8e6926c0ce130877886d4790
sinamo24 <138229031+sinamo24@users.noreply.github.com>
2023-10-22 16:37:04 +0200

    simplified

--- R/median_normalize.R
+++ R/median_normalize.R
@@ -14,10 +14,7 @@
 
 
 median_normalize <- function(data){
-  data_n <- data[, !colnames(data) %in% "row.number"]  # exclude ID column
-
-  data_n <- as.data.frame(lapply(data_n, function(col) col/median(col, na.rm = T)))
-  data_n <- cbind(row.number = data$row.number, data_n)  # add ID column back
-
-  return(data_n)
+  # median normalize all columns but the ID column
+  data[, !colnames(data) %in% "row.number"] <- as.data.frame(lapply(data[, !colnames(data) %in% "row.number"], function(col) col/median(col, na.rm = T)))
+  return(data)
 }
